62ee60feea03230b77a8096c07b157543fe54804
eymin1259 <eymin1259@gmail.com>
2023-05-14 19:30:44 +0900

    ui 패키지의 컴포넌트 사용

--- apps/snaps-editor/src/pages/index.tsx
+++ apps/snaps-editor/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { libHello } from "@snaps/lib";
+import { BlueButton } from "@snaps/ui";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 const inter = Inter({ subsets: ["latin"] });
@@ -10,6 +11,13 @@ export default function Home() {
         <div className={styles.description}>
           <p>{libHello()}</p>
         </div>
+        <BlueButton
+          onClick={() => {
+            console.log("BlueButton clicked");
+          }}
+        >
+          snaps/ui 버튼
+        </BlueButton>
       </main>
     </>
   );
