db3cfec14a4c6e37b1956e478297509350e5c9ea
eymin1259 <eymin1259@gmail.com>
2023-05-15 20:16:19 +0900

    editor ui 수정

--- apps/snaps-editor/src/pages/index.tsx
+++ apps/snaps-editor/src/pages/index.tsx
@@ -1,23 +1,23 @@
 import { libHello } from "@snaps/lib";
 import { BlueButton } from "@snaps/ui";
 import { Inter } from "next/font/google";
+import { useCallback } from "react";
 import styles from "@/styles/Home.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const onClickBlueBtn = useCallback(() => {
+    console.log("BlueButton clicked");
+  }, []);
+
   return (
     <>
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
-          <p>{libHello()}</p>
+          <p>editor {libHello()}</p>
         </div>
-        <BlueButton
-          onClick={() => {
-            console.log("BlueButton clicked");
-          }}
-        >
-          snaps/ui 버튼
-        </BlueButton>
+        <BlueButton onClick={onClickBlueBtn}>버튼1</BlueButton>
+        <BlueButton onClick={onClickBlueBtn}>버튼2</BlueButton>
       </main>
     </>
   );
